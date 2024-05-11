90a29b8857e00483e58e254d774253f775bf646e
cpu6502 <44790317+cpu6502@users.noreply.github.com>
2024-05-11 07:53:51 +0100

    Change config file path

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -46,12 +46,12 @@ static const int TOOLTIP_WRAP_THRESHOLD = 80;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
-#define QAPP_APP_NAME_SIGNET "Bitcoin-Qt-signet"
-#define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
+#define QAPP_ORG_NAME "Bytecoin"
+#define QAPP_ORG_DOMAIN "bytecoin.biz"
+#define QAPP_APP_NAME_DEFAULT "Bytecoin-Qt"
+#define QAPP_APP_NAME_TESTNET "Bytecoin-Qt-testnet"
+#define QAPP_APP_NAME_SIGNET "Bytecoin-Qt-signet"
+#define QAPP_APP_NAME_REGTEST "Bytecoin-Qt-regtest"
 
 /* One gigabyte (GB) in bytes */
 static constexpr uint64_t GB_BYTES{1000000000};
